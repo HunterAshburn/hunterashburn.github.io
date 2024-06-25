b66a1942afacb47a3ac495473a6bdc0e87524912
HunterAshburn <85366454+HunterAshburn@users.noreply.github.com>
2024-06-24 22:19:35 -0400

    sidebar hovering styles

--- assets/js/dynamic-sidebar.js
+++ assets/js/dynamic-sidebar.js
@@ -3,11 +3,53 @@ const listItems = sidebarList.querySelectorAll('li');
 const hrs = sidebarList.querySelectorAll('hr');
 const contentItems = document.querySelectorAll('.sidebar-content-item');
 
-const viewingColor = hexToRgb(getComputedStyle(root).getPropertyValue('--sidebar-viewing-color').trim());
+const viewingColorHex = getComputedStyle(root).getPropertyValue('--sidebar-viewing-color').trim();
+const viewingColor = hexToRgb(viewingColorHex);
 const notViewingColor = hexToRgb(getComputedStyle(root).getPropertyValue('--sidebar-not-viewing-color').trim());
+const darkTextColor = getComputedStyle(root).getPropertyValue('--dark-text-color').trim();
 
 const baselineFontSizeElement = document.getElementById('javascript-li-font-size-baseline');
 const fontSizeMultiplier = 1.2;
+
+const cssListItemHoverClass = 'sidebar-li-hover'
+const cssHoverTransitionSpeed = '0.15s'
+
+function addHoverStyle() {
+    this.classList.add(cssListItemHoverClass);
+    this.style.transition = 
+        'background-color ' + cssHoverTransitionSpeed + 
+        ', color ' + cssHoverTransitionSpeed + 
+        ', font-size ' + cssHoverTransitionSpeed;
+    this.style.color = darkTextColor;
+    this.style.fontSize = interpolateFontSize(this, 1) + 'px';
+    
+    const index = Array.from(listItems).indexOf(this);
+
+    if (index > 0) {
+        hrs[index - 1].style.transition = 'border-color ' + cssHoverTransitionSpeed;
+        hrs[index - 1].style.borderColor = viewingColorHex;
+    }
+
+    if (index < listItems.length - 1) {
+        hrs[index].style.transition = 'border-color ' + cssHoverTransitionSpeed;
+        hrs[index].style.borderColor = viewingColorHex;
+    }
+}
+
+function removeHoverStyle() {
+    this.classList.remove(cssListItemHoverClass);
+
+    const index = Array.from(listItems).indexOf(this);
+    updateSidebarItem(index);
+
+    if (index > 0) {
+        updateSidebarItemBorder(index);
+    }
+
+    if (index < listItems.length - 1) {
+        updateSidebarItemBorder(index + 1);
+    }
+}
 
 function interpolateColor(color1, color2, percentage) {
     let result = color1.slice();
@@ -64,14 +106,27 @@ function elementVisibilityPercentage(element) {
     }
 }
 
+function updateSidebarItem(i) {
+    visibilityPercentage = elementVisibilityPercentage(contentItems[i]);
+    listItems[i].style.color = interpolateColor(notViewingColor, viewingColor, visibilityPercentage);
+    listItems[i].style.fontSize = interpolateFontSize(listItems[i], visibilityPercentage) + 'px';
+}
+
+function updateSidebarItemBorder(i) {
+    hrs[i - 1].style.borderColor = interpolateColor(rgbStringToRgb(listItems[i - 1].style.color), rgbStringToRgb(listItems[i].style.color), 0.5);
+}
+
 function updateSidebar() {
     for (let i = 0; i < contentItems.length; i++) {
-        visibilityPercentage = elementVisibilityPercentage(contentItems[i]);
-        listItems[i].style.color = interpolateColor(notViewingColor, viewingColor, visibilityPercentage);
-        listItems[i].style.fontSize = interpolateFontSize(listItems[i], visibilityPercentage) + 'px';
 
-        if (i > 0) {
-            hrs[i - 1].style.borderColor = interpolateColor(rgbStringToRgb(listItems[i - 1].style.color), rgbStringToRgb(listItems[i].style.color), 0.5);
+        if (!listItems[i].classList.contains(cssListItemHoverClass)) {
+            listItems[i].style.transition = '';
+            updateSidebarItem(i);
+        
+            if (i > 0 && !listItems[i - 1].classList.contains(cssListItemHoverClass)) {
+                hrs[i - 1].style.transition = '';
+                updateSidebarItemBorder(i);
+            }
         }
     }
 }
@@ -79,6 +134,11 @@ function updateSidebar() {
 window.addEventListener('scroll', updateSidebar);
 
 window.addEventListener('resize', updateSidebar);
+
+listItems.forEach(function(item) {
+    item.addEventListener('mouseover', addHoverStyle);
+    item.addEventListener('mouseout', removeHoverStyle);
+});
 
 
 updateSidebar();
